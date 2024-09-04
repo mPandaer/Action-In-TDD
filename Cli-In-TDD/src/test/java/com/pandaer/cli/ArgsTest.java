8a16ad35ee1daa7f38b9cd400f89097c59907c41
mPandaer <liwenhaolx@gmail.com>
2024-09-04 19:34:47 +0800

    cli: 构造出两个测试用例

--- Cli-In-TDD/src/test/java/com/pandaer/cli/ArgsTest.java
+++ Cli-In-TDD/src/test/java/com/pandaer/cli/ArgsTest.java
@@ -1,5 +1,6 @@
 package com.pandaer.cli;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class ArgsTest {
@@ -17,7 +18,25 @@ public class ArgsTest {
         System.out.println(options.port());
     }
 
+    @Test
+    void should_example_1() {
+        Options options = Args.parse(Options.class,"-l","-p","8080","-d","/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080,options.port());
+        assertEquals("/usr/logs",options.dir());
+    }
+
+    @Test
+    void should_example_2() {
+        ListOptions options = Args.parse(ListOptions.class,"-l","-p","8080","-d","/usr/logs");
+        assertEquals(new String[]{"this","is","a","list"},options.group());
+        assertEquals(new int[]{1,2,3},options.decimals());
+    }
+
+
+
     static record Options(@Option("l") boolean logging,@Option("p")int port,@Option("d")String dir){}
 
+    static record ListOptions(@Option("g") String[] group,@Option("d") int[] decimals){}
 
 }
