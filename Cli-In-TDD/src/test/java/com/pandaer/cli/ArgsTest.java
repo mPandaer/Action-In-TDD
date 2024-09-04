724dc9328019be57270b92251be449dd4217e52b
mPandaer <liwenhaolx@gmail.com>
2024-09-04 19:25:32 +0800

    cli: 大致构思软件使用的方式

---
+++ Cli-In-TDD/src/test/java/com/pandaer/cli/ArgsTest.java
@@ -0,0 +1,19 @@
+package com.pandaer.cli;
+
+import org.junit.jupiter.api.Test;
+
+public class ArgsTest {
+
+    // -l -p 8080 -d /usr/logs
+
+    @Test
+    void should() {
+        Options options = Args.parse(Options.class,"-l","-p","8080","-d","/usr/logs");
+        System.out.println(options.logging());
+        System.out.println(options.port());
+    }
+
+    static record Options(@Option("l") boolean logging,@Option("p")int port,@Option("d")String dir){}
+
+
+}
