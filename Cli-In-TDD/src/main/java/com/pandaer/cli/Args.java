1a67cf3c4182a36ab43b514317c34adb11931a30
mPandaer <liwenhaolx@gmail.com>
2024-09-04 20:24:41 +0800

    cli: 修改代码，让正反例都通过

--- Cli-In-TDD/src/main/java/com/pandaer/cli/Args.java
+++ Cli-In-TDD/src/main/java/com/pandaer/cli/Args.java
@@ -2,13 +2,20 @@ package com.pandaer.cli;
 
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
 
 public class Args {
 
     public static <T> T parse(Class<T> clazz, String... args) {
-        Constructor<?> constructor = clazz.getDeclaredConstructors()[0];
+
         try {
-            return (T) constructor.newInstance(true);
+            List<String> argList = Arrays.stream(args).toList();
+            Constructor<?> constructor = clazz.getDeclaredConstructors()[0];
+            Parameter parameter = constructor.getParameters()[0];
+            Option option = parameter.getAnnotation(Option.class);
+            return (T) constructor.newInstance(argList.contains("-" + option.value()));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
