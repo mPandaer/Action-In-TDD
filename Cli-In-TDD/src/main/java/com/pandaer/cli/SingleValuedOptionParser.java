8b5fbb05acae6c863951889da3fabf93da36c46e
mPandaer <liwenhaolx@gmail.com>
2024-09-11 06:12:10 +0800

    cli: 重构 意图不明显 抽取方法，用方法名代替注释

--- Cli-In-TDD/src/main/java/com/pandaer/cli/SingleValuedOptionParser.java
+++ Cli-In-TDD/src/main/java/com/pandaer/cli/SingleValuedOptionParser.java
@@ -21,11 +21,23 @@ class SingleValuedOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> argList, String flag) {
         int index = argList.indexOf(flag);
         if (index == -1) return defaultValue;
-        if (index + 1 >= argList.size() || argList.get(index+1).startsWith("-")) throw new MissValueException(flag);
-        if (index + 2 < argList.size() &&
-                !(argList.get(index + 2).startsWith("-"))) throw new TooManyArgumentsException(flag);
+        if (isReachEndOfList(argList, index) || isFollowAFlag(argList, index)) throw new MissValueException(flag);
+        if (isSecondArgumentAFlag(argList, index)) throw new TooManyArgumentsException(flag);
         String value = argList.get(index + 1);
         return valueParser.apply(value);
+    }
+
+    private static boolean isSecondArgumentAFlag(List<String> argList, int index) {
+        return index + 2 < argList.size() &&
+                !(argList.get(index + 2).startsWith("-"));
+    }
+
+    private static boolean isFollowAFlag(List<String> argList, int index) {
+        return argList.get(index + 1).startsWith("-");
+    }
+
+    private static boolean isReachEndOfList(List<String> argList, int index) {
+        return index + 1 >= argList.size();
     }
 
 }
