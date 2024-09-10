284234210de1ac310c462a54d8897993c5b595fc
mPandaer <liwenhaolx@gmail.com>
2024-09-11 06:18:05 +0800

    cli: 抽取方法后，仍然意图不明显，可以选择重新换一种实现思路

--- Cli-In-TDD/src/main/java/com/pandaer/cli/SingleValuedOptionParser.java
+++ Cli-In-TDD/src/main/java/com/pandaer/cli/SingleValuedOptionParser.java
@@ -2,15 +2,12 @@ package com.pandaer.cli;
 
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
 class SingleValuedOptionParser<T> implements OptionParser<T> {
 
     Function<String, T> valueParser;
     T defaultValue;
-
-    private SingleValuedOptionParser(Function<String, T> valueParser) {
-        this.valueParser = valueParser;
-    }
 
     public SingleValuedOptionParser(T defaultValue,Function<String, T> valueParser) {
         this.valueParser = valueParser;
@@ -21,23 +18,14 @@ class SingleValuedOptionParser<T> implements OptionParser<T> {
     public T parse(List<String> argList, String flag) {
         int index = argList.indexOf(flag);
         if (index == -1) return defaultValue;
-        if (isReachEndOfList(argList, index) || isFollowAFlag(argList, index)) throw new MissValueException(flag);
-        if (isSecondArgumentAFlag(argList, index)) throw new TooManyArgumentsException(flag);
+        int nextFlagIndex = IntStream.range(index + 1, argList.size())
+                .filter(it -> argList.get(it).startsWith("-")).findFirst().orElse(argList.size());
+
+        List<String> values = argList.subList(index + 1, nextFlagIndex);
+        if (values.size() < 1) throw new MissValueException(flag);
+        if (values.size() > 1) throw new TooManyArgumentsException(flag);
         String value = argList.get(index + 1);
         return valueParser.apply(value);
-    }
-
-    private static boolean isSecondArgumentAFlag(List<String> argList, int index) {
-        return index + 2 < argList.size() &&
-                !(argList.get(index + 2).startsWith("-"));
-    }
-
-    private static boolean isFollowAFlag(List<String> argList, int index) {
-        return argList.get(index + 1).startsWith("-");
-    }
-
-    private static boolean isReachEndOfList(List<String> argList, int index) {
-        return index + 1 >= argList.size();
     }
 
 }
